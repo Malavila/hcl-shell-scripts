298278fcba4f5753ed5dfd034a3c78bd2e68dc9f
Malavika <root@ip-172-31-12-223.ap-south-1.compute.internal>
2021-08-12 11:41:14 +0000

    committed

---
+++ months-case.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+echo "Enter a number [0-11]"
+read n
+case $n in
+        0)
+                echo "January" ;;
+        1)
+                echo "February" ;;
+        2)
+                echo "March" ;;
+        3)
+                echo "April" ;;
+        4)
+                echo "May" ;;
+        5)
+                echo "June" ;;
+        6)
+                echo "July" ;;
+        7)
+                echo "August" ;;
+	8)
+		echo "September" ;;
+	9)
+		echo "October" ;;
+	10)
+		echo "November" ;;
+	11)
+		echo "December" ;;
+		
+esac
+~
+~
+
