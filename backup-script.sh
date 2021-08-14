5c0bde7db670dda9d8a41ea62543519435a851a4
malavika <malu4353@gmail.com>
2021-08-14 10:45:02 +0000

    dsdf

--- backup-script.sh
+++ backup-script.sh
@@ -3,4 +3,4 @@
 DATE=$(date +%S-%M-%H-%d-%m-%Y)
 BACKUP_DIR=/var/log/backup
 
-tar -czvf $BACKUP_DIR/backup-scripts-$DATE.tar.gz /var/log/jenkins
+tar -czvf $BACKUP_DIR/shell-scripts-$DATE.tar.gz /var/log/jenkins
